af4b46dfb6b75b0acc6502a288d162a0e41537a9
arongoncalves <168443295+arongoncalves@users.noreply.github.com>
2026-03-12 11:26:49 -0300

    Update banco-loja.sql

--- banco-loja.sql
+++ banco-loja.sql
@@ -5,3 +5,12 @@ CREATE TABLE Produtos (
     Comentarios TEXT
 );
 select * from Produtos;
+
+
+CREATE TABLE Itens (
+    Codigo_Item INT PRIMARY KEY,
+    Nome_Item VARCHAR(100) NOT NULL,
+    Preco_Unitario DECIMAL(10, 2) NOT NULL,
+    Descricao TEXT
+);
+SELECT * FROM Itens;
